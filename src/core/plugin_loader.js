1a2d5bc604b951b8629180ee6419ca0858ba134b
晚梦 <127290936+LateDreamXD@users.noreply.github.com>
2025-11-08 21:10:15 +0800

    fix(core): fix if data path not exists get error on delete plugin

--- src/core/plugin_loader.js
+++ src/core/plugin_loader.js
@@ -12,7 +12,7 @@ const config = LiteLoader.api.config.get("LiteLoader", default_config);
 function deletePlugin(slug) {
     try {
         const { plugin_path, data_path } = config.deleting_plugins[slug];
-        if (data_path) fs.rmSync(data_path, { recursive: true });
+        if (data_path && fs.existsSync(data_path)) fs.rmSync(data_path, { recursive: true });
         fs.rmSync(plugin_path, { recursive: true });
     }
     catch (error) {
