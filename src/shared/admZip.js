4eb4ebe29dce89fde61ba26e80cb0285bfdd7de8
晚梦 <127290936+LateDreamXD@users.noreply.github.com>
2025-11-12 00:45:34 +0800

    fix(shared): require path then join

--- src/shared/admZip.js
+++ src/shared/admZip.js
@@ -1,5 +1,5 @@
 export default (() => {
-    const major_node = path.join(process.resourcesPath, "app", "major.node");
+    const major_node = require('path').join(process.resourcesPath, "app", "major.node");
     require(major_node).load("internal_admzip", module);
     return exports.admZip.default;
 })();
