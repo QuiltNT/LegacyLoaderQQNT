7f1e3a554cba29d72e051f42a3d874e699e7b67f
晚梦 <127290936+LateDreamXD@users.noreply.github.com>
2025-11-14 19:46:39 +0800

    fix(shared): use esm export in cjs

--- src/shared/admZip.js
+++ src/shared/admZip.js
@@ -1,4 +1,4 @@
-export default (() => {
+module.exports = (() => {
     const major_node = require('path').join(process.resourcesPath, "app", "major.node");
     require(major_node).load("internal_admzip", module);
     return exports.admZip.default;
